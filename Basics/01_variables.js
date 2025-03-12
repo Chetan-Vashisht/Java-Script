2af00eb4e67ddcd56e5b7ea976ec596c5cdc8326
Chetan-Vashisht <chetanvashishtc@gmail.com>
2025-03-12 17:30:54 +0530

    variables and data type

---
+++ Basics/01_variables.js
@@ -0,0 +1,15 @@
+const userName = "Chetan Vashist"
+let userId = "1234"
+var userPass = "34343"
+userState = "Active"
+
+// not to use var because of problem in block level scope
+// const not reassignmend and redeclaration
+// let only reassighment possible
+
+
+// shortcut to print console.log() is just type log
+
+console.log(userName);
+console.table([userName, userId, userPass])
+
